2d80795a8447be3fe98afaea17d33b6f8209988c
Danilo <danilo@anycode.it>
2010-10-10 23:27:39 +0200

    commit per provare a mettere form nel widget

--- lib/template/CustomFields.php
+++ lib/template/CustomFields.php
@@ -25,25 +25,48 @@ class Doctrine_Template_CustomFields extends Doctrine_Template
     $this->addListener(new Doctrine_Template_Listener_CustomFields($this->_options));
   }
 
-  public function setUp()
-  {
-
-  }
-
   public function getCustomField($field)
   {
     $invoker = $this->getInvoker();
-    if(isset($invoker->custom_fields[$field]))
-        return $invoker->custom_fields[$field]['value'];
+
+    if( !($_ac_custom_fields = $invoker->option("_ac_custom_fields")) )
+    {
+      $_ac_custom_fields = $this->customFieldsCompile($invoker);
+    }
+
+    if(isset($_ac_custom_fields[$field]))
+        return $_ac_custom_fields[$field]['value'];
     else
         throw new Doctrine_Record_UnknownPropertyException();
   }
 
-  public function setCustomField($field,$value)
+  protected function customFieldsCompile($invoker)
+  {
+    if(isset($invoker->custom_fields))
+    {
+      foreach($invoker->custom_fields as $cf)
+      {
+        $_ac_custom_fields[$cf['label']] = $cf;
+      }
+
+      $invoker->option("_ac_custom_fields", $_ac_custom_fields);
+      return $_ac_custom_fields;
+    }
+  }
+
+  public function setCustomField($label,$value,$type="text")
   {
     $invoker = $this->getInvoker();
     $custom_fields = $invoker->custom_fields;
-    $custom_fields[$field] = $value;
+    $custom_fields[] = array("label"=>$label,"value"=>$value,"type"=>$type);
     $invoker->custom_fields = $custom_fields;
+
+    // if isset _ac_custom_fields option then add new custom fields to it
+    if(($_ac_custom_fields = $invoker->option("_ac_custom_fields")) )
+    {
+      $_ac_custom_fields[$label] = array("label"=>$label,"value"=>$value,"type"=>$type);
+      $invoker->option("_ac_custom_fields", $_ac_custom_fields);
+    }
+
   }
 }
